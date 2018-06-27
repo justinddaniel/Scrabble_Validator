44b1ffff669e48da0fad14cf16daf9f59d53fe09
Justin Daniel <justinddaniel@gmail.com>
2018-06-27 10:47:14 -0400

    Initial code;
    Validation file works for a string. Needs good image parser.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Scrabble_Validator
+
+This app is currently in development. Once functional it will allow a user to take a picture of a set of Scrabble Tiles from their smartphone and have the program return a message if they have extra/missing tiles or a complete Scrabble set. This can be especially useful for Scrabble tournaments where many sets are used or hand-me-downs, and tiles can occasionally get mixed up in other sets. 
+
+The message returned to the user will have complete information: it will tell how many of each letter are missing/extra or if the set is complete. It will thus go well beyond merely counting total tiles. 
+
+At the time of writing (Jun 27 2018) the validation.js file is fully functional and, given a data string of tile letters, returns an exact message of what is missing/extra. 
+
+The roadblock has been finding a smart image parser that can correctly convert an image of letters to an accurate data string. While the validation.js file has been coded to correct some mistakes, such as when the program interpret L as '1' or 'I', other mistakes such as ignoring a tile entirely or interpreting an H as '4 T' are more difficult to correct. 
+
+Once an acceptable image parser has been developed or found, it is then a matter of wiring up an external api where a picture taken can automatically return a text string that is then parsed in this app's backend. 
+Ideally, the way this app is intended to work with a user is the following:
+
+1) User takes a picture of a set of Scrabble tiles
+2) The picture is automatically routed to an external api with an image parser or, alternatively, its own image parser on the backend. 
+3) The image parser converts it to a text string that is then processed by validation.js
+4) validation.js turns the string into an interpolated return message that tells the user what tile(s) are extra or missing or validates the set as complete. (This programming step is complete although it may require refactoring depending on how the image parser behaves)
+5) User receives the return message. 
+
+Additional functionality can also be added later: it might also be fascinating and useful for the validator to look at a game in progress and validate the words played as legitimate scrabble words, for example. 
+
+This can be a very useful app for Scrabble players with additional development of an image parser and connection to an api. 
---
+++ validation.js
@@ -0,0 +1,69 @@
+//global variables
+
+let alphabetObject = {A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0, I: 0, J: 0, K: 0, L: 0, M: 0,
+	N: 0, O: 0, P: 0, Q: 0, R: 0, S: 0, T: 0, U: 0, V: 0, W: 0, X: 0, Y: 0, Z: 0}
+
+let completeScrabbleObject = {A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2,
+	N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1}
+
+//first function to sanitize the string, converting commmon mis-translations and eliminating noise might be useful. 
+
+function sanitizeString (string) {
+	string = string.replace(/[,. ]/g, "")   //image parsers tend to return man punctuation and spaces and these can be removed. 
+	string = string.replace(/[1l]/g, "I")   //corrects for common mistranslations of "I"
+	string = string.replace(/0/g, "O")      //corrects for common mistranslations of "O"
+	array = string.split("").map((letter) => {
+	    if (letter.charCodeAt(0) === 913) {       //corrects for common mistranslations of "A"
+	        letter = "A";
+	        return letter;
+	    }
+	    else if (letter.charCodeAt(0) === 914) {  //corrects for common mistranslations of "B"
+	    	letter = "B";
+	    	return letter;
+	    }
+	    else {return letter}
+	})
+	string = array.join("")
+	return string;
+} 
+
+
+
+//next function takes a sanitized string input and converts it to an object of key value/pairs: e.g. {a: 9, b:2, ...}
+
+function stringToObject (string) {
+	let lettersArray = string.split("")
+	lettersArray.forEach((key) => {
+		if (alphabetObject.hasOwnProperty(key.toUpperCase())) {
+			alphabetObject[key.toUpperCase()] += 1;
+		}
+	})
+	return alphabetObject;
+}
+
+//next function iterates over the keys/values of alphabetObject and subtracts the values of the completeScrabbleObject
+
+function validateScrabble (alphabetObject) {
+	Object.keys(alphabetObject).forEach((key) => {
+		alphabetObject[key] -= completeScrabbleObject[key]
+	})
+	return alphabetObject;
+}
+
+//final function takes the alphabetObject from validateScrabble function and identifies any extra/missing tiles or returns a message that set is complete
+function isValidScrabbleSet (alphabetObject) {
+	let message = "";
+	Object.keys(alphabetObject).forEach((key) => {
+		if (alphabetObject[key] > 0) {
+			message += `${alphabetObject[key]} extra ${key} (should have ${completeScrabbleObject[key]}) ` 
+		}
+		else if (alphabetObject[key] < 0) {
+			message += `${Math.abs(alphabetObject[key])} missing ${key} (should have ${completeScrabbleObject[key]}) ` 
+		}
+	})
+	return message === "" ? "Your Set is Complete!" : message  //final return message is either a validation of complete set or identification of missing/extra tiles
+}
+
+/* all functions tested and seem to work. Need to build in edge cases. Last function should probably
+take a different variable than alphabetObject since this would return as complete if it was passed
+in when first defined. */
