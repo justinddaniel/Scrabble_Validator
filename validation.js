1e44bddd6c092907fcafbe93a4adcf97ed83de15
Justin Daniel <justinddaniel@gmail.com>
2018-07-24 11:19:15 -0400

    alphabetObject resets after use

--- validation.js
+++ validation.js
@@ -11,6 +11,8 @@ let completeScrabbleObject = {A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I
 //first function prompts the user for string input based on what they got from an image parser
 
 function getUserInput() {
+	alphabetObject = {A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0, I: 0, J: 0, K: 0, L: 0, M: 0,
+	N: 0, O: 0, P: 0, Q: 0, R: 0, S: 0, T: 0, U: 0, V: 0, W: 0, X: 0, Y: 0, Z: 0} //reset from previous user input
 	string = window.prompt("Hello! please enter the tile letters you have. It is recommended that you use an image to text program and paste its output string here")
 	return sanitizeString(string);
 }
